af208b36825ec1ab408b177dea4f117fb184a59e
eh4git <ehirsch760@gmail.com>
2022-12-08 21:29:44 -0800

    readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Async Await Demo
+
+## Getting started
+
+Have Node.js installed and run:
+
+```
+node async
+```
+
+Check the terminal to see the results.
+
+## Have any Questions?
+
+Send them over to my Github email.
+
+## Contributing
+
+Do you have something that would make this demo better I would love to hear about it! Send me an email or fork and open a pull request!
--- async.js
+++ async.js
@@ -1,4 +1,4 @@
-//? Execute the following code in node to see example.
+//? Execute the following code in Node to see example.
 const wild = runWild();
 runControlled();
 
@@ -16,15 +16,14 @@ function runWild() {
   const third = mockAsync("I AM THIRD", 800);
   console.log(third);
 
-  //! Notice that these are all returning Promise Objects. We do not have access to the data yet. These console.logs() ran before the promise was resolved.
+  //* Notice that these are all returning Promise Objects. We do not have access to the data yet. These console.logs() ran before the promise was resolved.
 
-  //! We can resolve the promises one at a time, but this will not return the data in a predictable way. ***Check the terminal for the console.log()s***
+  //* We can resolve the promises one at a time, but this will not return the data in a predictable way. ***Check the terminal for the console.log()s***
   first.then(data => console.log(data));
   second.then(data => console.log(data));
   third.then(data => console.log(data));
 
-  //! To resolve all of the Promises we could use a Promise.all(). It will wait for the data to be returned before trying to access it. But we will have to "nest" our code into a .then() and we do not get the data until all of the promises are resolved.
-  //! Uncomment the Promise.all() to see one way we can retrieve the values
+  //* To resolve all of the Promises we could use a Promise.all(). It will wait for the data to be returned before trying to access it, but we will have to "nest" our code into a .then() and we do not get the data until all of the promises are resolved.
   return Promise.all([first, second, third]).then(data => {
     console.log(data);
   });
@@ -33,7 +32,7 @@ function runWild() {
 async function runControlled() {
   await wild;
   console.log("---------Start of the runControlled function---------");
-  //! We can await any Promise, in this demo we do not want this function to run until after the wild() function finishes. (so what happens in the terminal is a little more obvious)
+  //! We can await any Promise, in this demo we do not want this function to run until after the wild() function finishes. (so what happens in the terminal is a little more obvious, typically you would not await another function in this way)
 
   const first = await mockAsync("Now I AM FIRST.... RIGHT??", 1000);
   console.log(first);
@@ -42,5 +41,5 @@ async function runControlled() {
   const third = await mockAsync("Now I AM THIRD", 800);
   console.log(third);
 
-  //! Notice that all of these logs happen in order, and we have all of the data that is returned when the promise is resolved. That is because it waits for one line to finish completely before continuing to the next line. No need for a nested structure that a .then() would provide. async/await allows us to write code in a clear/concise and predictable way, without the need for nested functions.
+  //* Notice that all of these logs happen in order, and we have all of the data that is returned when the promise is resolved. That is because it waits for one line to finish completely before continuing to the next line. No need for a nested structure that a .then() would provide. async/await allows us to write code in a clear/concise and predictable way, without the need for nested functions.
 }
